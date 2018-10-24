041dcf83ef62907b0da386491544ae75c945cc0b
WeiJietao <13415156317@163.com>
2018-10-24 21:53:53 +0800

    更新用户中心界面

--- client/pages/organization/index.js
+++ client/pages/organization/index.js
@@ -1,66 +1,125 @@
 // pages/organization/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        myOrganList: [
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            },
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            },
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            }
+        ],
+        hotOrganList: [
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            },
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            },
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            }
+        ],
+        nearOrganList: [
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            },
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            },
+            {
+                img: './act1.jpg',
+                title: '世界末日小组',
+                detail: '鞍山市所所所所所所鞍山市所所所所所所鞍山市所所所所所所',
+                actAmount: '99+'
+            }
+        ]
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    }
 })
